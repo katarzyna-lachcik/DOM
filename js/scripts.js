420ffec7464e6f080e960219a2d40d5babd5cffc
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-26 14:41:55 +0100

    buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var button = document.getElementsByClassName('button');
-console.log(button);
-console.log(button.length);
-for (var i = 0; i < button.length; i++) {
-	alert(button[i].innerText);
+var buttons = document.getElementsByClassName('button');
+console.log(buttons);
+console.log(buttons.length);
+for (var i = 0; i < buttons.length; i++) {
+	alert(buttons[i].innerText);
 };
